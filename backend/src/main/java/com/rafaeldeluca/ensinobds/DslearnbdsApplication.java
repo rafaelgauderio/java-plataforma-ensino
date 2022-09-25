8a82c3d69310d710315da36a58d821f4877b1d73
Rafael De Luca <deluca1712@gmail.com>
2022-09-25 11:48:49 -0300

    Create backend project setup

---
+++ backend/src/main/java/com/rafaeldeluca/ensinobds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package com.rafaeldeluca.ensinobds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
