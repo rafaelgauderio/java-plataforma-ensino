87c44cba699bdabf3ba1908df0e3443ba652ad32
Rafael De Luca <deluca1712@gmail.com>
2022-10-15 21:46:12 -0300

    Method findById

--- backend/src/main/java/com/rafaeldeluca/ensinobds/repositories/UserRepository.java
+++ backend/src/main/java/com/rafaeldeluca/ensinobds/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rafaeldeluca.ensinobds.entities.User;
 
-public interface UserRepository extends JpaRepository <User, Long>{
+public interface UserRepository extends JpaRepository <User, Long> {
+	
+	User findByEmail(String email);
 
 }
