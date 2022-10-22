ccb0a0965c455fc8a9a8e443363ffb81ebb2e3fc
Rafael De Luca <deluca1712@gmail.com>
2022-10-22 12:26:35 -0300

    Pre-authorizing methods by user profile

--- backend/src/main/java/com/rafaeldeluca/ensinobds/resources/DeliverResource.java
+++ backend/src/main/java/com/rafaeldeluca/ensinobds/resources/DeliverResource.java
@@ -2,6 +2,7 @@ package com.rafaeldeluca.ensinobds.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +19,11 @@ public class DeliverResource {
 	@Autowired
 	private DeliverService deliverService;
 	
+	
+	//somente o instrutor e admin podem corrigir as tarefas, aluno não está autorizado a acessar esse endPoint
+	@PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")
 	@PutMapping(value = "/{id}")
-	public ResponseEntity <Void> save (@PathVariable Long id, @RequestBody DeliverRevisionDTO dto ) {
+	public ResponseEntity <Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDTO dto ) {
 		deliverService.saveRevision(id, dto);
 		return ResponseEntity.noContent().build();
 	
